d79681638cea6b96405ea6716ba0db931498cba3
hoffme <phjocoronel@gmail.com>
2021-01-25 21:45:05 -0300

    arreglando navegacion y agregando productos

--- public/js/navegacion.js
+++ public/js/navegacion.js
@@ -1,8 +1,8 @@
 function activacionNavegacion() {
     var elemento = document.getElementById("navegacion");
-    if (elemento.classList.contains('deplegada')) {
-        elemento.classList.remove('deplegada');
+    if (elemento.classList.contains('navegacion-abierta')) {
+        elemento.classList.remove('navegacion-abierta');
     } else {
-        elemento.classList.add('deplegada');
+        elemento.classList.add('navegacion-abierta');
     }
 }
